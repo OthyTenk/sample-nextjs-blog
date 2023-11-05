98655d3bb8bcac44b97bfd142b639b669664a7ac
Othy Tenk <othy.tenk@gmail.com>
2023-11-05 13:51:12 +0800

    adds basePath with production condition

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /**
  * @type {import('next').NextConfig}
  */
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
+    basePath: isProd?'sample-nextjs-blog':'',
   output: "export",
   distDir: 'dist',
   images:{
